4ada417c9d395860db3c11f5c6ac66ed1eed515e
Denis <d358468115905@gmail.com>
2020-12-19 20:51:51 +0200

    Modified the notify

--- notify.sh
+++ notify.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-for i in {0..60..1}
+START=1 # Начало отсчета
+STEP=1 # Указываем с каким шагом 
+echo -n "Введите количество секунд цифрами: "
+read NUMBERS
+
+echo -n "Ввдите текст сообщения: "
+read MESSAGE
+
+for i in $(eval echo "{$START..$NUMBERS..$STEP}")    
     do
           killall xfce4-notifyd
-          sleep 0.7
-          notify-send "Отключай Discord!!!" -t 0
-	  sleep 1.5
+          sleep 0.5
+          notify-send "$MESSAGE"
+          #notify-send $i
+          sleep 0.5
     done
